9f817f73eb2dd4bea2114a27e9d9e737eeecb904
Tatiana Sugina <internetova@yandex.ru>
2023-09-02 22:32:18 +0300

    Offstage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_navigation/pages/blue/blue_page.dart';
 import 'package:flutter_navigation/pages/green/green_page.dart';
@@ -32,9 +33,44 @@ class _MyAppState extends State<MyApp> {
         useMaterial3: true,
       ),
       home: Scaffold(
-        body: IndexedStack(
-          index: _currentIndex,
-          children: _pages,
+        body: Stack(
+          children: _pages
+          /// Метод из пакета collection
+          /// Iterable<R> mapIndexed<R>(R Function(int, Widget) convert)
+              .mapIndexed(
+            /// Offstage - это виджет Flutter, который позволяет управлять видимостью
+            /// (отображением или скрытием) своего потомка без удаления его из дерева виджетов.
+            /// В отличие от виджетов, которые просто скрываются, Offstage сохраняет своего
+            /// потомка в дереве виджетов и может быть полезным для оптимизации производительности
+            /// и управления видимостью в зависимости от определенных условий или состояний приложения.
+            //
+            // Offstage имеет один обязательный параметр:
+            //
+            // offstage: Булево значение, которое определяет, отображается ли потомок виджета
+            // (false) или скрыт (true).
+                (i, w) => Offstage(
+                  offstage: i != _currentIndex,
+                  /// TickerMode - это виджет Flutter, который позволяет управлять активацией и
+                  /// деактивацией анимаций и обновлений виджета. Он может быть полезным в случаях,
+                  /// когда вы хотите временно приостановить анимации или обновления виджета,
+                  /// чтобы оптимизировать производительность или предотвратить ненужные
+                  /// изменения состояния в определенных ситуациях.
+                  //
+                  // TickerMode имеет один обязательный параметр:
+                  //
+                  // enabled: Булево значение, которое определяет, активирован ли режим
+                  // тикера (true) или отключен (false).
+                  // Когда TickerMode установлен в false, все анимации и обновления виджета,
+                  // которые зависят от Ticker, будут приостановлены. Это может быть полезно,
+                  // например, при переключении между вкладками или экранами в приложении,
+                  // чтобы избежать лишних вычислений и обновлений.
+                  child: TickerMode(
+                    enabled: i == _currentIndex,
+                    child: w,
+                  ),
+                ),
+              )
+              .toList(),
         ),
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: _currentIndex,
